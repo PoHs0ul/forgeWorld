a3798a48927b1b7ee85154ec63612302663cbbc4
ShadowbladeXI <qays.100@gmail.com>
2017-11-05 20:32:10 +0100

    Added useless comment

--- ForgeWorld2/src/main/Main.java
+++ ForgeWorld2/src/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	
 	public static void main(String[] args) {
+		//start of program
 		GameMechanics gameMec = new gameMechanics.GameMechanics();
 		new gameGraphics.GameGraphics(gameMec);
 	}
