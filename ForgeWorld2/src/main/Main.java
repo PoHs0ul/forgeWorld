58a91fac7f7b1813b1fb9af9239f6196e9e5f0e1
ShadowbladeXI <qays.100@gmail.com>
2017-11-05 20:35:15 +0100

    removed useless comment

--- ForgeWorld2/src/main/Main.java
+++ ForgeWorld2/src/main/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
 	
 	public static void main(String[] args) {
-		//start of program
 		GameMechanics gameMec = new gameMechanics.GameMechanics();
 		new gameGraphics.GameGraphics(gameMec);
 	}
